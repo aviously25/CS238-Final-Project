e756a8d079ed9975d9438f9cce3aeb4886c65ba1
Adrian Gamarra <adriangamarra2018@gmail.com>
2023-03-05 12:28:11 -0800

    Added reward and collisions with non_parking

--- environment.py
+++ environment.py
@@ -2,12 +2,15 @@ from CARLO.geometry import Point
 from CARLO.agents import parkingSpot
 import numpy as np
 import random
+from copy import deepcopy
         
 class environment:
     def __init__(self, w):
         self.parkingSpots = []
         self.target = None
+        self.car = None
         self.w = w
+        self.controller = None
 
     def setUp(self):
         self.parkingSpots = [parkingSpot(self.w, point, direction) for point, direction in [
@@ -26,3 +29,23 @@ class environment:
     
     def get_target(self):
         return self.target
+    
+    def collide_non_target(self, car = None):
+        if(car is None):
+            car = self.car
+        for parking in self.parkingSpots:
+            if(parking is not self.target and car.is_colliding(parking)):
+                return True
+            
+        return False
+    
+    def reward_function(self, car = None):
+        if(car is None):
+            car = self.car
+
+        value = (self.collide_non_target(car=car) * -10000)
+        value += (car.park_dist(self.target, car=car)*-1000)
+        value += (self.car.get_offset(self.target.heading) *-1)
+
+
+        return value
